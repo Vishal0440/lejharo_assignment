3542fc7e8f0e40ca31a810f9f6d76927513b31c4
vishal0440 <prasadvishal70@gmail.com>
2024-06-24 10:33:34 +0530

    final assignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,81 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const Navbar = () => {
-  const [showSidebar,setShowSidebar] = useState(false)
+  const [showSidebar, setShowSidebar] = useState(false);
 
   return (
     <>
-    <div className="home-page">
-      <div class="header">
-        <div className="top-header">
-          <div>
-            <a href="#"><img src="https://www.lejhro.com/lejhro_logo_white.png" alt="logo" /></a>
-          </div>
-          <div className="menu">
+      <div className="home-page">
+        <div className="header">
+          <div className="top-header">
+            <a href="#">
+              <img
+                src="https://www.lejhro.com/lejhro_logo_white.png"
+                width="30%"
+                alt="logo"
+              />
+            </a>
+            <div className="menu">
               <a href="#">Innovation</a>
               <a href="#">Business Service</a>
               <a href="#">Financial Services</a>
               <a href="#">BootCamp</a>
-          </div>
-          <div class="hamburger">
-            <i id="hamburger" onClick={()=>setShowSidebar(true)} class="fa-solid fa-bars"></i>
+            </div>
+            <a className="menubar" href="#">
+              <i
+                onClick={() => setShowSidebar(true)}
+                class="fa-solid fa-bars"
+              ></i>
+            </a>
           </div>
         </div>
+        <h2 className="blogs">Blogs</h2>
       </div>
 
-        <h2 className='blogs'>Blogs</h2>
-    </div>
-
-
-    <div class={`${showSidebar? 'open':'close'} sidebar`}>
-      <div class="logo">
-      <a href="#"><img src="https://www.lejhro.com/lejhro_logo_blue.png" alt="logo" /></a>
-      <i id="hamburger" onClick={()=>setShowSidebar(false)} class="fa-solid fa-xmark"></i>
+      <div className={`${showSidebar ? "open" : "close"} sidebar`}>
+        <div className="sidebar-header">
+          <div className="sidebar-logo">
+            <a href="#">
+              <img
+                src="https://www.lejhro.com/lejhro_logo_blue.png"
+                alt="logo"
+              />
+            </a>
+          </div>
+          <div className="sidebar-close">
+            <a href="#">
+              <i onClick={() => setShowSidebar(false)}
+                className="fa-solid fa-xmark"
+              ></i>
+            </a>
+          </div>
+        </div>
+        <div className="sidebar-menu">
+          <a href="#">Innovation</a>
+          <a href="#">BootCamp</a>
+          <a href="#">Business Services</a>
+          <a href="#">Financial Services</a>
+          <a href="#">About</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Blog</a>
+        </div>
+        <div className="sidebar-social-icon">
+          <a href="#">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-youtube"></i>
+          </a>
+        </div>
       </div>
-      <a href="#"><span>Innovation</span></a>
-      <a href="#"><span>BootCamp</span></a>
-      <a href="#"><span>Business Services</span></a>
-      <a href="#"><span>Financial Services</span></a>
-      <a href="#"><span>About</span></a>
-      <a href="#"><span>Contact Us</span></a>
-      <a href="#"><span>Blog</span></a>
-      <div className="social-icons">
-              <a href="#"><i class="fa-brands fa-twitter"></i></a>
-              <a href="#"><i class="fa-brands fa-facebook"></i></a>
-              <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-              <a href="#"><i class="fa-brands fa-youtube"></i></a>
-            </div>
-    </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
